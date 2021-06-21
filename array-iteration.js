342e3934fb4d8521b58c003b084d63bc5174d6bc
samarth-systango <swath@isystango.com>
2021-06-21 21:40:53 +0530

    basic forEach loop implemented

--- array-iteration.js
+++ array-iteration.js
@@ -1,1 +1,9 @@
+    let namesArray=["samarth", "abhilash", "ankit", "suresh", "ramesh"];
+    console.log(namesArray);
+    //forEach() is method used on array but it is also a known as a 
+    //loop which is used to iterate over the elements
+    //it basically takes a callback function
+    namesArray.forEach((name)=>{
+        console.log(name);
+    });
     
